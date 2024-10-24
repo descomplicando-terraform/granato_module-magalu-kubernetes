c7e57853b64aaa735b006dcb498477a4213a2ab7
natalia.gassis <natalia.gassis@luizalabs.com>
2024-10-24 20:56:03 -0300

    feat: refactor environment variable export in GitHub Actions workflow

--- versions.tf
+++ versions.tf
@@ -2,24 +2,11 @@ terraform {
   required_providers {
     mgc = {
       source = "magalucloud/mgc"
+      version = "0.27.1"
     }
     local = {
       source  = "hashicorp/local"
       version = "2.5.2"
     }
   }
-  backend "s3" {
-    bucket                      = "granato-tf"
-    key                         = "kubernetes-nataliagranato.tfstate"
-    secret_key                  = "env.MGC_OBJ_KEY_SECRET"
-    access_key                  = "env.MGC_OBJ_KEY_ID"
-    region                      = "env.MGC_REGION"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-    endpoints = {
-      s3 = "https://br-se1.magaluobjects.com/"
-    }
-  }
-}
+
