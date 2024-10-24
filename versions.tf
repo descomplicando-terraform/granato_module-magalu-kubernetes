e888ac7f295186d13acaa4f61d148a0821baa845
natalia.gassis <natalia.gassis@luizalabs.com>
2024-10-24 20:21:40 -0300

    chore: remove unused terraform workflows and files

--- versions.tf
+++ versions.tf
@@ -8,4 +8,18 @@ terraform {
       version = "2.5.2"
     }
   }
+  backend "s3" {
+    bucket                      = "granato-tf"
+    key                         = "kubernetes-nataliagranato.tfstate"
+    secret_key                  = var.mgc_obj_key_secret
+    access_key                  = var.mgc_obj_key_id
+    region                      = var.mgc_region
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    endpoints {
+      s3 = "https://br-se1.magaluobjects.com/"
+    }
+  }
 }
