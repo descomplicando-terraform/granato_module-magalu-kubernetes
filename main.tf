4562256942f30dede9bc8339f86ab8807e0dd639
natalia.gassis <natalia.gassis@luizalabs.com>
2024-11-04 19:19:10 -0300

    refactor: remover arquivo providers.tf e atualizar recursos no main.tf e outputs.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,24 @@
-# Criando um cluster
-resource "mgc_kubernetes_cluster" "cluster_with_nodepool" {
+# Criando um cluster com um nodepool
+resource "mgc_kubernetes_cluster" "cluster" {
   name                 = var.cluster_name
   version              = var.kubernetes_version
   enabled_server_group = false
   description          = var.cluster_description
 }
 
-# Tempo de espera para o cluster ficar ativo
-# Ajuste o tempo conforme necessário
-resource "time_sleep" "wait_5_minutes" {
-  depends_on      = [mgc_kubernetes_cluster.cluster_with_nodepool]
-  create_duration = var.timer_duration
-}
-
-resource "random_string" "this" {
-  length  = 8
-  special = false
-  upper   = false
-  numeric = true # Atualizado para usar `numeric`
-}
-
 # Criando um nodepool
 resource "mgc_kubernetes_nodepool" "nataliagranato" {
-  depends_on   = [time_sleep.wait_5_minutes]
-  name         = "${mgc_kubernetes_cluster.cluster_with_nodepool.name}-nodepool-${random_string.this.result}"
-  cluster_id   = mgc_kubernetes_cluster.cluster_with_nodepool.id
-  flavor_name  = var.node_pools.default.flavor
-  replicas     = var.node_pools.default.min_replicas
-  min_replicas = var.node_pools.default.min_replicas
-  max_replicas = var.node_pools.default.max_replicas
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Timer para esperar o cluster ficar ativo
-resource "time_sleep" "wait_for_cluster" {
-  depends_on      = [mgc_kubernetes_cluster.cluster_with_nodepool]
-  create_duration = "5m" # Ajuste o tempo conforme necessário
+  depends_on  = [mgc_kubernetes_cluster.cluster]
+  name        = "nataliagranato"
+  cluster_id  = mgc_kubernetes_cluster.cluster.id
+  flavor_name = var.nodepool_flavor_name
+  replicas    = var.nodepool_replicas
 }
 
 # Pegar o kubeconfig do cluster usando o output do cluster_id
 data "mgc_kubernetes_cluster_kubeconfig" "cluster" {
-  depends_on = [time_sleep.wait_for_cluster]
-  cluster_id = mgc_kubernetes_cluster.cluster_with_nodepool.id
+  depends_on = [mgc_kubernetes_cluster.cluster]
+  cluster_id = mgc_kubernetes_cluster.cluster.id
 }
 
 # Salvar o kubeconfig em um arquivo local
--- variables.tf
+++ variables.tf
@@ -1,64 +1,41 @@
 variable "cluster_name" {
-  description = "Nome do Cluster"
+  description = "Cluster name"
   type        = string
-  default     = "nataliagranato"
+  default     = "mgc-cluster"
 }
 
 variable "kubernetes_version" {
-  description = "Versão do Kubernetes"
+  description = "Kubernetes version"
   type        = string
   default     = "v1.30.2"
 }
 
 variable "cluster_description" {
-  description = "Descrição do Cluster"
+  description = "Cluster description"
   type        = string
-  default     = "Um cluster de Kubernetes gerenciado pela Magalu Cloud."
+  default     = "A Kubernetes cluster managed by Magalu Cloud."
+}
+
+variable "nodepool_name" {
+  description = "Nodepool name"
+  type        = string
+  default     = "mgc-nodepool"
+}
+
+variable "nodepool_replicas" {
+  description = "Number of nodepool replicas"
+  type        = number
+  default     = 1
+}
+
+variable "nodepool_flavor_name" {
+  description = "Nodepool flavor"
+  type        = string
+  default     = "cloud-k8s.gp1.small"
 }
 
 variable "timer_duration" {
-  description = "Duração do Timer"
+  description = "Timer duration"
   type        = string
-  default     = "5m"
-}
-
-variable "mgc_api_key" {
-  type        = string
-  description = "Chave da Magalu Cloud"
-}
-
-variable "mgc_obj_key_id" {
-  type        = string
-  description = "ID da Chave do Object Storage"
-}
-
-variable "mgc_obj_key_secret" {
-  type        = string
-  description = "Secret da Chave do Object Storage"
-}
-
-variable "mgc_region" {
-  type        = string
-  description = "Região da Magalu Cloud"
-  default     = "br-se1"
-}
-
-variable "node_pools" {
-  description = "Mapa de Node Pools"
-  type = map(object({
-    flavor       = string
-    min_replicas = number
-    max_replicas = number
-  }))
-  default = {
-    default = {
-      flavor       = "cloud-k8s.gp1.medium"
-      min_replicas = 2
-      max_replicas = 5
-    }
-  }
-  validation {
-    condition     = alltrue([for np in var.node_pools : np.min_replicas < np.max_replicas])
-    error_message = "min_replicas deve ser menor que max_replicas para todos os pools de nós."
-  }
+  default     = "15m"
 }
