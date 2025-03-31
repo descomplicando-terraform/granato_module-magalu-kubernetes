34864b2927de49e2e71f8da551b1cd5b18c50d98
Natália Granato <nataliagranato@ufmg.br>
2025-03-31 18:35:33 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     mgc = {
       source  = "magalucloud/mgc"
-      version = "~> 0.27.0" # Permite atualizações dentro da versão 0.27.x
+      version = "0.32.2"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.5.0" # Permite atualizações dentro da versão 2.5.x
+      version = "2.5.2"
     }
   }
 }
